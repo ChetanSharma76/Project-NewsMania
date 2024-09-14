be731c95b73707cdb241dfa56ccd16e753da9df1
ChetanSharma76 <159418211+ChetanSharma76@users.noreply.github.com>
2024-09-14 23:40:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function reload() {
 async function fetchNews(query) {
     const res = await fetch(`${url}${query}&apiKey=${API_KEY}`);
     const data = await res.json();
-    bindData(data.articles);
+    bindData(data.articles);             //bascially data.articles is an array of all the articles that is fetched and parsed
 }
 
 function bindData(articles) {
@@ -29,11 +29,11 @@ function bindData(articles) {
 
     cardsContainer.innerHTML = "";
 
-    articles.forEach((article) => {
-        if (!article.urlToImage) return;
+    articles.forEach((article) => {                  //for each article we are creating and copying the same template and filling that card with the data
+        if (!article.urlToImage) return;             //urlToImage renders the image from the url
         const cardClone = newsCardTemplate.content.cloneNode(true);
-        fillDataInCard(cardClone, article);
-        cardsContainer.appendChild(cardClone);
+        fillDataInCard(cardClone, article);          //empty card and the article from the articles array
+        cardsContainer.appendChild(cardClone);        //then the card is ready and formed so just put it in the container
     });
 }
 
@@ -54,7 +54,7 @@ function fillDataInCard(cardClone, article) {
     newsSource.innerHTML = `${article.source.name} · ${date}`;
 
     cardClone.firstElementChild.addEventListener("click", () => {
-        window.open(article.url, "_blank");
+        window.open(article.url, "_blank");        //opens a new window related to that particular news
     });
 }
 
@@ -62,9 +62,9 @@ let curSelectedNav = null;
 function onNavItemClick(id) {
     fetchNews(id);
     const navItem = document.getElementById(id);
-    curSelectedNav?.classList.remove("active");
-    curSelectedNav = navItem;
-    curSelectedNav.classList.add("active");
+    curSelectedNav?.classList.remove("active");     //if not null remove from the active element 
+    curSelectedNav = navItem;                       //make the current selected Nav the curr element     
+    curSelectedNav.classList.add("active");         //make this element active
 }
 
 const searchButton = document.getElementById("search-button");
@@ -81,8 +81,8 @@ searchButton.addEventListener("click", () => {
 const modebtn=document.querySelector('.modebtn');
 const body=document.body;
 
-modebtn.addEventListener('click',()=>{
-
+modebtn.addEventListener('click',()=>{                      //switching to the dark mode as soon as the button is clicked
+   
 
     body.classList.toggle('dark-mode');
 
